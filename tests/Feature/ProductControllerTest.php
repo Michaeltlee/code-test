0a30a3cfce87fb1d23296c73c03340288b5af703
Michael Lee <mlee@wilbergroup.com>
2022-05-21 14:52:53 -0500

    Implement an authentication system
    Implement an authentication system so that the API knows which of the users is making the request. Ensure that an authorized user is making the request. In the event of an unauthorized user, an error should be thrown.

--- tests/Feature/ProductControllerTest.php
+++ tests/Feature/ProductControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -11,12 +12,25 @@ class ProductControllerTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
+    public function cannot_create_a_product_if_not_authenticated()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->expectException(\Illuminate\Auth\AuthenticationException::class);
+
+        $this->postJson(route('product.create'), [
+            'name'        => 'first product',
+            'description' => 'Description of the first product',
+            'price'       => 2000,
+        ]);
+    }
+
+    /** @test */
     public function can_create_a_product()
     {
         $this->withoutExceptionHandling();
 
-        // TODO: authenticate
-        $response = $this->postJson(route('product.create'), [
+        $response = $this->actingAs(User::factory()->create())->postJson(route('product.create'), [
             'name'        => 'first product',
             'description' => 'Description of the first product',
             'price'       => 2000,
@@ -36,8 +50,7 @@ class ProductControllerTest extends TestCase
             'price' => 2000,
         ]);
 
-        // TODO: authenticate
-        $response = $this->putJson(route('product.update', $product), [
+        $response = $this->actingAs(User::factory()->create())->putJson(route('product.update', $product), [
             'name'        => 'NEW: first product',
             'description' => 'Description of the NEW first product',
             'price'       => 3000,
@@ -62,8 +75,7 @@ class ProductControllerTest extends TestCase
             'price' => 2000,
         ]);
 
-        // TODO: authenticate
-        $response = $this->deleteJson(route('product.delete', $product));
+        $response = $this->actingAs(User::factory()->create())->deleteJson(route('product.delete', $product));
 
         $response->assertStatus(200);
 
@@ -79,8 +91,7 @@ class ProductControllerTest extends TestCase
             'price' => 2000,
         ]);
 
-        // TODO: authenticate
-        $response = $this->getJson(route('product.show', $product));
+        $response = $this->actingAs(User::factory()->create())->getJson(route('product.show', $product));
 
         $response->assertStatus(200)->assertJson([
             'name'        => $product->name,
@@ -97,8 +108,7 @@ class ProductControllerTest extends TestCase
 
         Product::factory()->count(5)->create();
 
-        // TODO: authenticate
-        $response = $this->getJson(route('product.index'));
+        $response = $this->actingAs(User::factory()->create())->getJson(route('product.index'));
 
         $response->assertStatus(200);
     }
